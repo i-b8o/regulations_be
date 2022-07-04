e275d9c7e540268bf7c42b7a180de897dedb6263
bogach-ivan <blackview1617@gmail.com>
2022-07-04 22:59:35 +0300

    sdfdsfsf

--- app/internal/domain/regulation/model/model.go
+++ app/internal/domain/regulation/model/model.go
@@ -5,6 +5,6 @@ import "time"
 type Regulation struct {
 	RegulationId   int        `json:"regulation_id"`
 	RegulationName string     `json:"regulation_name"`
-	CreatedAt      time.Time  `json:"created_at"`
-	UpdatedAt      *time.Time `json:"updated_at"`
+	CreatedAt      time.Time  `json:"created_at,omitempty"`
+	UpdatedAt      *time.Time `json:"updated_at,omitempty"`
 }
